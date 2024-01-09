14e5487f6c85f78ab3a687c4ef25fc2e22bbfe5a
Roeland Kindt <RoelandCEKindt@gmail.com>
2024-01-09 11:50:09 +0000

    version 1.14-1

--- R/WFO.acceptable.match.R
+++ R/WFO.acceptable.match.R
@@ -13,11 +13,47 @@ WFO.acceptable.match <- function(
   x$submitted <- stringr::str_replace(x$submitted, pattern="us$", replacement="a")
   x$matched <- stringr::str_replace(x$matched, pattern="us$", replacement="a")
 
+  x$submitted <- stringr::str_replace(x$submitted, pattern="is$", replacement="e")
+  x$matched <- stringr::str_replace(x$matched, pattern="is$", replacement="e")
+
   x$submitted <- stringr::str_replace(x$submitted, pattern="-", replacement="")
   x$matched <- stringr::str_replace(x$matched, pattern="-", replacement="")
 
+  x$submitted <- stringr::str_replace(x$submitted, pattern=intToUtf8(215), replacement="")
+  x$matched <- stringr::str_replace(x$matched, pattern=intToUtf8(215), replacement="")
+
+  x$submitted <- stringr::str_replace_all(x$submitted, pattern="j", replacement="i")
+  x$matched <- stringr::str_replace_all(x$matched, pattern="j", replacement="i")
+
   x$submitted <- stringr::str_replace_all(x$submitted, pattern="ii", replacement="i")
   x$matched <- stringr::str_replace_all(x$matched, pattern="ii", replacement="i")
+
+  x$submitted <- stringr::str_replace_all(x$submitted, pattern="tt", replacement="t")
+  x$matched <- stringr::str_replace_all(x$matched, pattern="tt", replacement="t")
+
+  x$submitted <- stringr::str_replace_all(x$submitted, pattern="ll", replacement="l")
+  x$matched <- stringr::str_replace_all(x$matched, pattern="ll", replacement="l")
+
+  x$submitted <- stringr::str_replace_all(x$submitted, pattern="rr", replacement="r")
+  x$matched <- stringr::str_replace_all(x$matched, pattern="rr", replacement="r")
+
+  x$submitted <- stringr::str_replace_all(x$submitted, pattern="mm", replacement="m")
+  x$matched <- stringr::str_replace_all(x$matched, pattern="mm", replacement="m")
+
+  x$submitted <- stringr::str_replace_all(x$submitted, pattern="nn", replacement="n")
+  x$matched <- stringr::str_replace_all(x$matched, pattern="nn", replacement="n")
+
+  x$submitted <- stringr::str_replace_all(x$submitted, pattern="ff", replacement="f")
+  x$matched <- stringr::str_replace_all(x$matched, pattern="ff", replacement="f")
+
+  x$submitted <- stringr::str_replace_all(x$submitted, pattern="hh", replacement="h")
+  x$matched <- stringr::str_replace_all(x$matched, pattern="hh", replacement="h")
+
+  x$submitted <- stringr::str_replace_all(x$submitted, pattern="dd", replacement="d")
+  x$matched <- stringr::str_replace_all(x$matched, pattern="dd", replacement="d")
+
+  x$submitted <- stringr::str_replace_all(x$submitted, pattern="dt", replacement="d")
+  x$matched <- stringr::str_replace_all(x$matched, pattern="dt", replacement="d")
 
   if (no.vowels == TRUE) {
     x$submitted <- stringr::str_replace_all(x$submitted, pattern="[aeiouy]", replacement="")
--- R/new.backbone.R
+++ R/new.backbone.R
@@ -4,36 +4,46 @@ new.backbone <- function(
     scientificNameAuthorship="scientificNameAuthorship",
     acceptedNameUsageID=NULL, taxonomicStatus=NULL)
 {
+  
+  x <- data.frame(x)
+  for (j in 1:ncol(x)) {x[, j] <- as.character(x[, j])}
+  
+  out <- data.frame(taxonID = x[, taxonID],
+                    scientificName = x[, scientificName],
+                    scientificNameAuthorship = x[, scientificNameAuthorship])
+  
+  if (is.null(acceptedNameUsageID)) {
+    out$acceptedNameUsageID <- as.character("", nrow(out))
+  }else{
+    out <- data.frame(out,
+                      acceptedNameUsageID = x[, acceptedNameUsageID])
+  }
+  
+  if (is.null(taxonomicStatus)) {
+    out$taxonomicStatus <- as.character("Accepted", nrow(out))
+    out[out$acceptedNameUsageID != "", "taxonomicStatus"] <- "Synonym"
+  }else{
+    out <- data.frame(out,
+                      taxonomicStatus = x[, taxonomicStatus])
+  }
 
-x <- data.frame(x)
-for (j in 1:ncol(x)) {x[, j] <- as.character(x[, j])}
-
-out <- data.frame(taxonID = x[, taxonID],
-                  scientificName = x[, scientificName],
-                  scientificNameAuthorship = x[, scientificNameAuthorship])
-
-if (is.null(acceptedNameUsageID)) {
-  out$acceptedNameUsageID <- as.character("", nrow(out))
-}else{
-  out <- data.frame(out,
-                    acceptedNameUsageID = x[, acceptedNameUsageID])
-}
-
-if (is.null(taxonomicStatus)) {
-  out$taxonomicStatus <- as.character("Accepted", nrow(out))
-  out[out$acceptedNameUsageID != "", "taxonomicStatus"] <- "Synonym"
-}else{
-  out <- data.frame(out,
-                    taxonomicStatus = x[, taxonomicStatus])
-}
-
-keep.columns <- names(x) %in% c("taxonID",
-                                "scientificName", "scientificNameAuthorship",
-                                "acceptedNameUsageID", "taxonomicStatus"
-                                ) == FALSE
-
-out <- data.table::data.table(out, x[, keep.columns])
-
-return(out)
-
+  keep.columns <- names(x) %in% c("taxonID",
+                                  "scientificName", "scientificNameAuthorship",
+                                  "acceptedNameUsageID", "taxonomicStatus"
+  ) == FALSE
+  
+  out <- data.table::data.table(out, x[, keep.columns])
+  
+  # modified January 2024 to deal with self-referencing acceptedNameUsageID
+  selfies <- out$taxonID == out$acceptedNameUsageID
+  selfies[is.na(selfies)==TRUE] <- FALSE
+  n.self <- nrow(out[selfies, ])
+  
+  if (n.self > 0) {
+    message(paste("Self-referencing was detected for", n.self, "records, so emptying acceptedNameUsageID for these."))
+    out[selfies, "acceptedNameUsageID"] <- ""
+  }
+  
+  return(out)
+  
 }
